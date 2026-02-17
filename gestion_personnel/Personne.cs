6b13b2afba2faec05a8f1e27a72b51eab2b29bdb
1234litt <raoulnaoussi9@gmail.com>
2026-02-17 14:12:25 +0100

    classe Personne

---
+++ gestion_personnel/Personne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_personnel
+{
+    class Personne
+    {
+        //Attributs de la classe personne
+        string Nom;
+        int Age;
+        //Constructeur de la classe Personne
+        public Personne(string nom, int age)
+        {
+            Nom = nom;
+            Age = age;
+        }
+        // Méthode AfficherInfo
+        public void AfficherInfo()
+        {
+            Console.WriteLine($"Nom:{Nom}, Age:{Age}");
+
+        }
+    }
+}
---
+++ gestion_personnel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_personnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne personnel1 = new Personne("Paola", 25);
+            Personne personnel2 = new Personne("Michelle", 26);
+
+            //Appel de la methode afficherinfo dans le programme principale
+            personnel1.AfficherInfo();
+            personnel2.AfficherInfo();
+
+            Console.Read();
+        }
+    }
+}
