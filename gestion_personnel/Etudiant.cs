7bcad060232def8db12bd105df79df821d06eb84
Raoul Yong <raoulnaoussi9@gmail.com>
2026-02-17 16:41:37 +0100

    Ajout de la classe Etudiant

---
+++ gestion_personnel/Etudiant.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_personnel
+{
+    class Etudiant
+    {
+        private string nom;
+        private int age;
+        private float moyenne;
+
+        //Propriétés publiques de l'attribut nom
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        //Propriétés publiques de l'attribut age
+        public int Age
+        {
+            get { return age; }
+            set 
+            { if (value > 0) age = value; 
+              else throw new ArgumentException("L'âge doit être positif !"); 
+            }
+        }
+        //Propriétés publiques de l'attribut moyenne
+        public float Moyenne
+        {
+            get { return moyenne; }
+            set
+            {
+                if (value >= 0 && value <= 20)
+                    moyenne = value;
+                else
+                    Console.WriteLine("Erreur : la moyenne doit être comprise entre 0 et 20 !");
+            }   
+        }
+        //constructeur
+        public Etudiant(string nom, int age, float moyenne)
+        {
+            Nom = nom;
+            Age = age;
+            Moyenne = moyenne;
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom : {Nom}, Age : {Age}, Moyenne : {Moyenne}");
+        }
+
+    }
+}
--- gestion_personnel/Program.cs
+++ gestion_personnel/Program.cs
@@ -14,21 +14,36 @@ namespace gestion_personnel
             Personne personnel2 = new Personne("Michelle", 26);
             //Attributs de la classe rectangle
             Rectangle rect1 = new Rectangle(5, 6);
-            //Attributs de la clase compte
+            //Attributs de la clase CompteBancaire
             CompteBancaire compte = new CompteBancaire();
+            //Attributs de la clase Etudiant
+            Etudiant etudiant1 = new Etudiant("Alice", 20, 15);
 
             //Appel de la methode afficherinfo dans le programme principale
             personnel1.AfficherInfo();
             personnel2.AfficherInfo();
             //Appel des methode CalculerAire
             Console.WriteLine($"Aire : {rect1.CalculerAire()}");
-            Console.WriteLine($"Aire : {rect1.CalculerPerimetre()}");
+            Console.WriteLine($"Périmetre : {rect1.CalculerPerimetre()}");
 
-            
+            //Opération sur la classe CompteBancaire
             compte.Deposer(500);
             compte.Retirer(200);
             compte.Retirer(400); // Solde insuffisant
-            
+
+            //Appel de la methode Afficherinfo
+            etudiant1.AfficherInfos();
+
+            // Test différents cas
+            etudiant1.Moyenne = 18;   // OK
+            etudiant1.AfficherInfos();
+
+            etudiant1.Moyenne = 25;   // Erreur : trop élevé
+            etudiant1.Moyenne = -3;   // Erreur : trop bas
+
+            // Vérification finale
+            etudiant1.AfficherInfos();
+
 
             Console.Read();
         }
